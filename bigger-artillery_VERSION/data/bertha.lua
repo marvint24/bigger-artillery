112e86183dbe8b3a41023bb146851fa2f2eb5d7e
marvint24 <marvin.terme@gmail.com>
2024-11-02 17:21:15 +0100

    balance update

--- bigger-artillery_VERSION/data/bertha.lua
+++ bigger-artillery_VERSION/data/bertha.lua
@@ -4,53 +4,11 @@ bertha.icon = "__bigger-artillery__/graphics/artillery-turret.png"
 bertha.stack_size = 1
 bertha.place_result = "big-artillery-turret"
 
-data:extend{bertha}
+data:extend { bertha }
 
 local recipe = table.deepcopy(data.raw["recipe"]["artillery-turret"])
 recipe.enabled = false
 recipe.name = "big-artillery-turret"
-recipe.ingredients ={
-  {
-    name ="steel-plate", 
-    amount=500,
-    type = "item"
-  },
-  {
-    name = "concrete", 
-    amount = 250,
-    type = "item"
-  },
-  {
-    name = "iron-gear-wheel", 
-    amount = 440,
-    type = "item"
-  },
-  {
-    name = "advanced-circuit", 
-    amount = 120,
-    type = "item"
-  },
-  {
-    name = "processing-unit", 
-    amount = 100,
-    type = "item"
-  },
-  {
-    name = "electric-engine-unit", 
-    amount = 32,
-    type = "item"
-  },
-  {
-    name = "low-density-structure", 
-    amount = 50,
-    type = "item"
-  },
-  {
-    name = "artillery-turret", 
-    amount = 1,
-    type = "item"
-  }
-}
 recipe.results = {
   {
     amount = 1,
@@ -59,52 +17,149 @@ recipe.results = {
   }
 }
 
-data:extend{recipe}
+recipe.ingredients = {
+  {
+    amount = 600,
+    name = "steel-plate",
+    type = "item"
+  },
+  {
+    amount = 600,
+    name = "concrete",
+    type = "item"
+  },
+  {
+    amount = 400,
+    name = "iron-gear-wheel",
+    type = "item"
+  },
+  {
+    amount = 200,
+    name = "advanced-circuit",
+    type = "item"
+  },
+  {
+    name = "processing-unit",
+    amount = 100,
+    type = "item"
+  },
+  {
+    name = "electric-engine-unit",
+    amount = 32,
+    type = "item"
+  },
+  {
+    name = "low-density-structure",
+    amount = 50,
+    type = "item"
+  },
+  {
+    name = "artillery-turret",
+    amount = 3,
+    type = "item"
+  }
+}
 
+if mods["space-age"] then
+  recipe.ingredients = {
+    {
+      amount = 400,
+      name = "iron-gear-wheel",
+      type = "item"
+    },
+    {
+      amount = 100,
+      name = "processing-unit",
+      type = "item"
+    },
+    {
+      amount = 600,
+      name = "tungsten-plate",
+      type = "item"
+    },
+    {
+      amount = 600,
+      name = "refined-concrete",
+      type = "item"
+    },
+    {
+      name = "electric-engine-unit", 
+      amount = 32,
+      type = "item"
+    },
+    {
+      name = "low-density-structure", 
+      amount = 50,
+      type = "item"
+    },
+    {
+      name = "artillery-turret",
+      amount = 3,
+      type = "item"
+    }
+  }
+end
+
+data:extend { recipe }
 
 local tech = table.deepcopy(data.raw["technology"]["artillery"])
 tech.name = "big-artillery"
-tech.icon = "__bigger-artillery__/graphics/artillery.png"  
+tech.icon = "__bigger-artillery__/graphics/artillery.png"
 tech.effects =
-    {
-      {
-        type = "unlock-recipe",
-        recipe = "big-artillery-turret"
-      }
-    }
-tech.prerequisites = {"artillery","utility-science-pack","low-density-structure","electric-engine"}
-tech.unit =
-    {
-      ingredients =
-      {
-        {"automation-science-pack", 1},
-        {"logistic-science-pack", 1},
-        {"chemical-science-pack", 1},
-        {"military-science-pack", 1},
-        {"utility-science-pack", 1}
-      },
-      time = 30,
-      count = 2000
-    }
+{
+  {
+    type = "unlock-recipe",
+    recipe = "big-artillery-turret"
+  }
+}
+tech.prerequisites = { "artillery" }
 
-data:extend{tech}
+tech.unit = {
+  ingredients = {
+    { "automation-science-pack", 1 },
+    { "logistic-science-pack",   1 },
+    { "chemical-science-pack",   1 },
+    { "military-science-pack",   1 },
+    { "utility-science-pack",    1 }
+  },
+  time = 30,
+  count = 4000
+}
+
+if mods["space-age"] then
+  tech.unit = {
+    ingredients = {
+      { "automation-science-pack",  1 },
+      { "logistic-science-pack",    1 },
+      { "military-science-pack",    1 },
+      { "chemical-science-pack",    1 },
+      { "utility-science-pack",     1 },
+      { "space-science-pack",       1 },
+      { "metallurgic-science-pack", 1 }
+    },
+    time = 30,
+    count = 3000
+  }
+end
+
+data:extend { tech }
 
 local gun = table.deepcopy(data.raw["gun"]["artillery-wagon-cannon"])
 gun.name = "big-artillery-wagon-cannon"
 
 --Scale muzzle-flash-shifting
 for k, v in pairs(gun.attack_parameters.projectile_creation_parameters) do
-  v[2][1]=v[2][1]*2.1
-  v[2][2]=v[2][2]*2.1
+  v[2][1] = v[2][1] * 2.1
+  v[2][2] = v[2][2] * 2.1
 end
 
-data:extend{gun}
+data:extend { gun }
 
-local guisetting={
+local guisetting = {
   type = "custom-input",
   name = "close-bb-menu",
   key_sequence = "E",
   alternative_key_sequence = "ESCAPE"
 }
 
-data:extend{guisetting}
+data:extend { guisetting }
